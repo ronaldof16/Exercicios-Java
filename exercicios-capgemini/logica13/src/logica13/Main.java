fa14f1b161dd02866e7f31d0a83d3c64bac973af
ronaldof16 <ronaldof16@hotmail.com>
2023-03-22 17:23:28 -0300

    Resolução do exercício logica13

---
+++ exercicios-capgemini/logica13/src/logica13/Main.java
@@ -0,0 +1,24 @@
+package logica13;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+		int x = scan.nextInt();
+		
+		if(x > 10) {
+			System.out.println( "O número digitado é maior que 10!");
+		} else {
+			System.out.println("O número digitado é menor que 10");
+		}
+		
+		scan.close();
+
+	}
+
+}
