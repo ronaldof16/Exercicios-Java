a5438772ff6945d1b0979b67c566789ac4fd54c0
ronaldof16 <ronaldof16@hotmail.com>
2023-03-24 11:54:22 -0300

    Resolução do exercício logica18

---
+++ exercicios-capgemini/logica18/src/logica18/Main.java
@@ -0,0 +1,33 @@
+package logica18;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		for(int i = 0; i < 2; i++) {
+			
+			System.out.print("Digite o nome da pessoa: ");
+			String nome = scan.nextLine();
+			System.out.print("Digite a idade: ");
+			int idade = scan.nextInt();
+			
+			if(idade >= 18) {
+				System.out.println(nome + " é maior de idade!");
+			} else {
+				System.out.println(nome + " é menor de idade!");
+			}
+			
+			scan.nextLine();
+		}
+		
+		scan.close();
+
+	}
+
+}
