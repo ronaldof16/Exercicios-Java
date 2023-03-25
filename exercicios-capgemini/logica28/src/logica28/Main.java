a452e73eb14c530a68f166169d00e28929976d2a
ronaldof16 <ronaldof16@hotmail.com>
2023-03-25 01:12:05 -0300

    Resoluçao do exrcicio logica27

---
+++ exercicios-capgemini/logica28/src/logica28/Main.java
@@ -0,0 +1,62 @@
+package logica28;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		float totalDesconto = 0;
+		float totalPago = 0;
+		System.out.print("Digite o preço do veículo: ");
+		float preco = scan.nextFloat();
+		
+		
+		while(preco != 0) {
+			
+			char combustivel;
+			float desconto = 0;
+			
+			System.out.print("Digite o combustível do ceículo: G - gasolina / D - diesel / A - álcool: ");
+			combustivel = scan.next().charAt(0);
+			
+			if(combustivel == 'G') {
+				desconto = preco * (21 / 100.0f);
+				preco = preco - desconto;
+				System.out.println("Valor do desconto: " + String.format("%.2f", desconto));
+				System.out.println("Preço final do veículo: " + String.format("%.2f", preco));
+			} else if(combustivel == 'A') {
+				desconto = preco * (25 / 100.0f);
+				preco = preco - desconto;
+				System.out.println("Valor do desconto: " + String.format("%.2f", desconto));
+				System.out.println("Preço final do veículo: " + String.format("%.2f", preco));
+			} else if(combustivel == 'D') {
+				desconto = preco * (14 / 100.0f);
+				preco = preco - desconto;
+				System.out.println("Valor do desconto: " + String.format("%.2f", desconto));
+				System.out.println("Preço final do veículo: " + String.format("%.2f", preco));
+			} else {
+				System.out.println("Tipo de combustível inválido!");
+			}
+			
+			totalDesconto = totalDesconto + desconto;
+			totalPago  = totalPago + preco;
+			
+			
+			System.out.print("Digite o preço do veículo: ");
+			preco = scan.nextFloat();
+			
+		}
+		
+		System.out.println("Total de  descontos: " + String.format("%.2f", totalDesconto));
+		System.out.println("Total pago pelos clientes: " + String.format("%.2f", totalPago));
+		
+		scan.close();
+
+	}
+
+}
