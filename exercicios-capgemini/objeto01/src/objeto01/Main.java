bf59a55dd09abad1b6d78b56bfae79ce31a45421
ronaldof16 <ronaldof16@hotmail.com>
2023-03-29 12:42:50 -0300

    Resolução do exercício objeto01

--- exercicios-capgemini/objeto01/src/objeto01/Main.java
+++ exercicios-capgemini/objeto01/src/objeto01/Main.java
@@ -1,10 +1,27 @@
 package objeto01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		Pessoa pessoa = new Pessoa();
+		
+		System.out.print("Digite o nome: ");
+		pessoa.setnome(scan.nextLine());
+		System.out.print("Digite o ano de nascimento: ");
+		pessoa.setanoNascimento(scan.nextInt());
+		System.out.print("Digite o ano atual: ");
+		pessoa.setanoAtual(scan.nextInt());
+		System.out.print("Digite a altura: ");
+		pessoa.setaltura(scan.nextFloat());
+		
+		pessoa.imprimirDados();
+		System.out.println("A idade é: " + pessoa.calcularIdade());
+		
+		scan.close();
 	}
 
 }
--- exercicios-capgemini/objeto01/src/objeto01/Pessoa.java
+++ exercicios-capgemini/objeto01/src/objeto01/Pessoa.java
@@ -1,38 +1,56 @@
 package objeto01;
 
-import java.util.Date;
-
 public class Pessoa {
 	
-	private String nome;
-	private Date dataNascimento;
-	private float altura;
-	
-	public String getnome() {
-		return nome;
-	}
-	
-	public void setnome(String nome) {
-		this.nome = nome;
-	}
-	
-	public Date getdataNascimento() {
-		return dataNascimento;
-	}
-	
-	public void setdataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	
-	public float getaltura() {
-		return altura;
-	}
-	
-	public void setaltura(float altura) {
-		this.altura = altura;
-	}
+	 private String nome;
+	 private int anoNascimento;
+	 private int anoAtual;
+	 float altura;
 	
 	
+	  public String getnome() {
+		  return nome; 
+	  }
+	  
+	  public void setnome(String nome) {
+		  this.nome = nome; 
+	  }
+	  
+	  public int getanoNascimento() {
+		  return anoNascimento; 
+	  }
+	  
+	  public void setanoNascimento(int anoNascimento) {
+		  this.anoNascimento = anoNascimento; 
+	  }
+	  
+	  public int getanoAtual() {
+		  return anoAtual;
+	  }
+	  
+	  public void setanoAtual(int anoAtual) {
+		  this.anoAtual = anoAtual;
+	  }
+	  
+	  public float getaltura() {
+		  return altura; 
+	  }
+	  
+	  public void setaltura(float altura) {
+		  this.altura = altura; 
+	  }
+	 
+	
+	  public void imprimirDados() {
+		  System.out.println("Nome: " + nome);
+		  System.out.println("Data de nascimento: " + anoNascimento);
+		  System.out.println("Ano atual: " + anoAtual);
+		  System.out.println("Altura: " + altura);
+	  }
+	  
+	  public int calcularIdade() {
+		  return anoAtual - anoNascimento;
+	  }
 	
 
 }
