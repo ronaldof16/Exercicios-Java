80631ddc6c6e48a03eecb611255edfc4d1ae6f8e
ronaldof16 <ronaldof16@hotmail.com>
2023-04-08 11:19:23 -0300

    Conclusão da resolução do exercío 03

--- exercicios-capgemini/objeto03/src/objeto03/BombaCombustivel.java
+++ exercicios-capgemini/objeto03/src/objeto03/BombaCombustivel.java
@@ -3,8 +3,50 @@ package objeto03;
 public class BombaCombustivel {
 	
 	private char tipo;
-	private float valor;
-	private float quantidade;
+	private float valorLitro;
+	private float quantidadeBomba;
 	
+	public BombaCombustivel(char tipo, float valorLitro, float quantidadeBomba) {
+		this.tipo = tipo;
+		this.valorLitro = valorLitro;
+		this.quantidadeBomba = quantidadeBomba;
+	}
+	
+	public char getTipo() {
+		return tipo;
+	}
+	
+	public float getValorLitro() {
+		return valorLitro;
+	}
+	
+	public float getQuantidadeBomba() {
+		return quantidadeBomba;
+	}
+	
+	public void alterarTipo(char tipo) {
+		this.tipo = tipo;
+	}
+	
+	public void alterarValorLitro(float valorLitro) {
+		this.valorLitro = valorLitro;
+	}
+	
+	public void alterarQuantidadeBomba(float quantidadeBomba) {
+		this.quantidadeBomba = quantidadeBomba;
+	}
+	
+	public float abastecerPorValor(float valor) {
+		float quantidadeAbastecida = valor / valorLitro;
+		quantidadeBomba += quantidadeAbastecida; 
+		return quantidadeAbastecida;
+		
+	}	
+	
+	public float abastecerPorLitro(float quantidadeLitro) {
+		float valorPago = quantidadeLitro * valorLitro;
+		quantidadeBomba += quantidadeLitro;
+		return valorPago; 
+	}
 	
 }
--- exercicios-capgemini/objeto03/src/objeto03/Main.java
+++ exercicios-capgemini/objeto03/src/objeto03/Main.java
@@ -1,10 +1,26 @@
 package objeto03;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		BombaCombustivel bomba = new BombaCombustivel('G', 5.29f, 8.0f);
+		
+		System.out.print("Digite o valor a ser abastecido: ");
+		bomba.abastecerPorValor(scan.nextFloat());
+		System.out.println("Quantidade Atualizada: " + bomba.getQuantidadeBomba());
+		System.out.println();
+		System.out.print("Digite a quantidade a ser abastecida: ");
+		bomba.abastecerPorLitro(scan.nextFloat());
+		System.out.println("Quantidade Atualizada: " + bomba.getQuantidadeBomba());
+		
+		scan.close();
 	}
 
 }
