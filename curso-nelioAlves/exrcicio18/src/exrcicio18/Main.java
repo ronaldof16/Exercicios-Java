ac1337f217425e63837191bdbe1c9c83e9af06d4
ronaldof16 <ronaldof16@hotmail.com>
2023-04-21 16:31:05 -0300

    Limpando os dados da classe Main

--- curso-nelioAlves/exrcicio18/src/exrcicio18/Main.java
+++ curso-nelioAlves/exrcicio18/src/exrcicio18/Main.java
@@ -1,20 +1,12 @@
 package exrcicio18;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		List<String> nome = new ArrayList<>();
 		
-		nome.add("ni");
-		nome.add("ze");
-		
-		for(String n : nome) {
-			System.out.println(n);
-		}
 
 	}
 
