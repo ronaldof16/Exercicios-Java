55ca986b6f4d2a8c4e2969462fd10f176e018ed9
ronaldof16 <ronaldof16@hotmail.com>
2023-04-25 11:46:42 -0300

    Criação dos métodos getters e setters e dos construtores

--- curso-nelioAlves/exrcicio18/src/exrcicio18/Funcionario.java
+++ curso-nelioAlves/exrcicio18/src/exrcicio18/Funcionario.java
@@ -2,8 +2,48 @@ package exrcicio18;
 
 public class Funcionario {
 	
-	private int id;
+	private Integer id;
 	private String nome;
-	private double salario;
+	private Double salario;
+	
+	public Funcionario(Integer id, String nome, Double salario) {
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+	}
+	
+	public Funcionario() {	
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double porcentagem) {
+		Double aumento = porcentagem * salario / 100;
+		salario += aumento;
+	}
+	
+	public String toString() {
+		return id + ", " + nome + ", " + salario;
+	}
+	
+	
 
 }
