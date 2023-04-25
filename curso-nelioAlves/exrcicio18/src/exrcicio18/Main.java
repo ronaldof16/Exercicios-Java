13514f54af55184e88baa14e475e9dc84e816ea1
ronaldof16 <ronaldof16@hotmail.com>
2023-04-25 11:48:33 -0300

    Criação da lista e do laço for para fazer o cadastro dos funcionários

--- curso-nelioAlves/exrcicio18/src/exrcicio18/Main.java
+++ curso-nelioAlves/exrcicio18/src/exrcicio18/Main.java
@@ -1,13 +1,48 @@
 package exrcicio18;
 
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.print("Quantos funcionários vão ser registrados? ");
+		int n = scan.nextInt();
+		List<Funcionario> lista = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println();
+			System.out.println("Funcionário #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			int id = scan.nextInt();
+			
+			while (hasId(lista, id)) {
+				System.out.print("Id já cadastrado! Digite outro: ");
+				id = scan.nextInt();
+			}
+			
+			System.out.print("Nome: ");
+			scan.nextLine();
+			String nome = scan.nextLine();
+			System.out.print("Salário: ");
+			double salario = scan.nextDouble();
+			lista.add(new Funcionario(id, nome, salario));
+		}
+		
+		System.out.println(lista);
+		
+		
+		scan.close();
+	}
 
+	private static boolean hasId(List<Funcionario> lista, int id) {
+		return false;
 	}
 
 }
