49f305d4c1becf7df9473ae8213fbf088c7c78f9
ronaldof16 <ronaldof16@hotmail.com>
2023-04-27 23:51:50 -0300

    Iniciando a resolução do exercício 19

---
+++ curso-nelioAlves/exercicio19/src/exercicio19/Main.java
@@ -0,0 +1,19 @@
+package exercicio19;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int m = scan.nextInt();
+		int n = scan.nextInt();
+		int[][] matriz = new int[m][n]; 
+		
+		
+		scan.close();
+	}
+
+}
