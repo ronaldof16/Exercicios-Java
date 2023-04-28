ba8487cfc64371d7036a31f34e71d035363c2fb0
ronaldof16 <ronaldof16@hotmail.com>
2023-04-28 10:41:42 -0300

    Conclusão da resolução do exercício

--- curso-nelioAlves/exercicio19/src/exercicio19/Main.java
+++ curso-nelioAlves/exercicio19/src/exercicio19/Main.java
@@ -10,8 +10,35 @@ public class Main {
 		
 		int m = scan.nextInt();
 		int n = scan.nextInt();
-		int[][] matriz = new int[m][n]; 
+		int[][] matriz = new int[m][n];
 		
+		for(int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				matriz[i][j] = scan.nextInt();
+			}
+		}
+		
+		int x = scan.nextInt();
+		
+		for(int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if(matriz[i][j] == x) {
+					System.out.println("Posição " + i + "," + j + ":");
+					if(j > 0) {
+						System.out.println("Esquerda: " + matriz[i][j - 1]);
+					}
+					if(i > 0) {
+						System.out.println("Em cima:" + matriz[i - 1][j]);
+					}
+					if (j < matriz[i].length-1) {
+						System.out.println("Direita: " + matriz[i][j + 1]);
+					}
+					if (i < matriz.length-1) {
+						System.out.println("Down: " + matriz[i + 1][j]);
+					}
+				}
+			}
+		}
 		
 		scan.close();
 	}
