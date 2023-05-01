6569d8b887a6921dd694e4f0e9a0b6931c763aca
ronaldof16 <ronaldof16@hotmail.com>
2023-05-01 17:31:57 -0300

    Convertendo data para texto com o método format

---
+++ curso-nelioAlves/exemploDataHora02/src/exemploDataHora02/Main.java
@@ -0,0 +1,35 @@
+package exemploDataHora02;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		LocalDate d04 = LocalDate.parse("2023-01-22");
+		LocalDateTime d05 = LocalDateTime.parse("1990-07-02T21:26:05");
+		Instant d06 = Instant.parse("2023-04-29T09:50:12Z");
+		
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+		
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d06 = " + fmt3.format(d06));
+		
+		System.out.println("d05 = " + fmt4.format(d05));
+		System.out.println("d05 = " + d05.format(fmt4));
+		
+		System.out.println("d06 = " + fmt5.format(d06));
+		
+
+	}
+
+}
