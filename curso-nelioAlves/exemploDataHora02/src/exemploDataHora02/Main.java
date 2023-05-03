a6b8fecc2c7fb83e6f6679bbdddef337ed575ee3
ronaldof16 <ronaldof16@hotmail.com>
2023-05-03 08:33:57 -0300

    Revisando os métodos ISO_DATE_TIME e ISO_INSTANT

--- curso-nelioAlves/exemploDataHora02/src/exemploDataHora02/Main.java
+++ curso-nelioAlves/exemploDataHora02/src/exemploDataHora02/Main.java
@@ -21,12 +21,14 @@ public class Main {
 		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 		
 		System.out.println("d04 = " + d04.format(fmt1));
-		System.out.println("d05 = " + d05.format(fmt2));
-		System.out.println("d06 = " + fmt3.format(d06));
+		System.out.println("d04 = " + fmt1.format(d04));
 		
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + d05.format(fmt1));
 		System.out.println("d05 = " + fmt4.format(d05));
 		System.out.println("d05 = " + d05.format(fmt4));
 		
+		System.out.println("d06 = " + fmt3.format(d06));
 		System.out.println("d06 = " + fmt5.format(d06));
 		
 
