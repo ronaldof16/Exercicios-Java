e8e6c556abd163b425f7966698b0a7c3ba3ca401
ronaldof16 <ronaldof16@hotmail.com>
2023-05-16 09:47:40 -0300

    Criação dos métodos

--- curso-nelioAlves/composicao03/src/entities/Order.java
+++ curso-nelioAlves/composicao03/src/entities/Order.java
@@ -1,5 +1,53 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enuns.OrderStatus;
+
 public class Order {
+	
+	private Date moment;
+	private OrderStatus status;
+	
+	private List<Product> items = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public List<Product> getItems() {
+		return items;
+	}
+
+	public void addItem(Product product) {
+		items.add(product);
+	}
+	
+	public void removeItem(Product product) {
+		items.remove(product);
+	}
+	
 
 }
