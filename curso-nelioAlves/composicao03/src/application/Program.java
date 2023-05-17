5611cde52a403df83d212d69b393e532c0664ec0
ronaldof16 <ronaldof16@hotmail.com>
2023-05-17 09:56:19 -0300

    Iniciando a aplicação com os dados do cliente

--- curso-nelioAlves/composicao03/src/application/Program.java
+++ curso-nelioAlves/composicao03/src/application/Program.java
@@ -1,10 +1,41 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.OrderItem;
+import entities.Product;
+import entities.enuns.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter cliente data: ");
+		System.out.print("Name: ");
+		String name = scan.nextLine();
+		System.out.print("Email: ");
+		String email = scan.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(scan.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(scan.next());
+		
+		
+		scan.close();
 	}
 
 }
